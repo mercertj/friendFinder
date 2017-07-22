5657b7c740c6525e5e83eafa1c6ef229fb515466
Thomas Mercer <tmercer74@gmail.com>
2017-07-21 17:17:52 -0700

    outstanding changes before applying .gitignore

---
+++ apiRoutes.js
@@ -0,0 +1,14 @@
+app.get("/api/friends", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.post("/api/survey", function(req, res) {
+  var newUser = req.body;
+  newcharacter.routeName = newUser.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newUser);
+
+  characters.push(newUser);
+
+  res.json(newUser);
+});
---
+++ server.js
@@ -0,0 +1,225 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var connection = connection;
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Get all characters
+app.get("/all", function(req, res) {
+  res.json(characters);
+});
+
+var friendList = 
+{
+  "name": "Joe Smith",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ],
+  "dependencies": {
+    "express": "^4.15.3"
+  }
+}
+
+{
+  "name": "Roger White",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ],
+  "dependencies": {
+    "express": "^4.15.3"
+  }
+}
+
+{
+  "name": "Jack Miller",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+    3,
+    2,
+    1,
+    1,
+    2,
+    3,
+    2,
+    5,
+    3,
+    4
+  ],
+  "dependencies": {
+    "express": "^4.15.3"
+  }
+}
+
+{
+  "name": "Ferris Bueller",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+    5,
+    5,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    4
+  ],
+  "dependencies": {
+    "express": "^4.15.3"
+  }
+}
+
+{
+  "name": "Jackie Treehorn",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ],
+  "dependencies": {
+    "express": "^4.15.3"
+  }
+}
+
+{
+  "name": "Jeff Lebowski",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+    2,
+    1,
+    4,
+    4,
+    2,
+    2,
+    2,
+    5,
+    3,
+    1
+  ],
+  "dependencies": {
+    "express": "^4.15.3"
+  }
+}
+
+{
+  "name": "Pat Davis",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
+  ],
+  "dependencies": {
+    "express": "^4.15.3"
+  }
+}
+
+{
+  "name": "Linda Coltrane",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+    5,
+    1,
+    3,
+    3,
+    5,
+    4,
+    2,
+    1,
+    4,
+    3
+  ],
+  "dependencies": {
+    "express": "^4.15.3"
+  }
+}
+{
+  "name": "Sandy Nelson",
+  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores": [
+    5,
+    3,
+    2,
+    4,
+    5,
+    5,
+    1,
+    5,
+    4,
+    1
+  ],
+  "dependencies": {
+    "express": "^4.15.3"
+  }
+}
